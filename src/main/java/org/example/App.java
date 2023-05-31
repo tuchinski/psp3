45930c848fbae3d19792dfe8eaec3349cb98b1fe
leonardo.tuchinski <leonardo.tuchinski@softtek.com>
2023-05-31 18:19:05 -0300

    finalizando entrada

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,29 +1,72 @@
 package org.example;
 
 
-public class App 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class App
 {
     public static void main( String[] args )
     {
+
+        if (args.length != 1){
+            System.out.println("Atenção!\nPara executar o programa, informe o caminho absoluto do arquivo com os dados na seguinte formatação" +
+                    "\n<variavel_x1>;<variavel_y1>" +
+                    "\n<variavel_x2>;<variavel_y2>" +
+                    "\n<variavel_xn>;<variavel_yn>");
+            return;
+        }
+        BufferedReader buff;
         RegressaoLinear r = new RegressaoLinear();
 
-        r.addNoLista(new No(130d,15d));
-        r.addNoLista(new No(650d,69.9));
-        r.addNoLista(new No(99d,6.5));
-        r.addNoLista(new No(150d,22.4));
-        r.addNoLista(new No(128d,28.4));
-        r.addNoLista(new No(302d,65.9));
-        r.addNoLista(new No(95d,19.4));
-        r.addNoLista(new No(945d,198.7));
-        r.addNoLista(new No(368d,38.8));
-        r.addNoLista(new No(961d,138.2));
+        String caminhoArquivos = args[0];
+
+        try {
+            buff = new BufferedReader(new FileReader(caminhoArquivos));
+        } catch (FileNotFoundException e) {
+            System.out.println("Arquivo não encontrado!");
+            return;
+        }
+
+        while(true) {
+            try {
+                String[] linha = buff.readLine().split(";");
+                r.addNoLista(new No(Double.parseDouble(linha[0]), Double.parseDouble(linha[1])));
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            } catch (NullPointerException e){
+                break;
+            }
+        }
+
+        System.out.println("Regressão: " + r.calculaRegressao());
+        System.out.println("MedidaCorrelacao: " + r.medidaCorrelacao());
+
+        Scanner s = new Scanner(System.in);
+
+        System.out.println("Digite o valor para o calculo da estimativa: ");
+        double estimativa = s.nextDouble();
+        System.out.println("Calculo Estimativa = " + r.calculoEstimativa(estimativa));
+
+        while(true){
+            System.out.println("Caso queira outra estimativa, informar o valor. Caso contrário, digite qualquer letra.");
+            try {
+                estimativa = s.nextDouble();
+                System.out.println("Calculo Estimativa = " + r.calculoEstimativa(estimativa));
+            } catch (InputMismatchException ex){
+                break;
+            }
+        }
 
 
-        System.out.println(r.calculaRegressao());
 
-        System.out.println("r.medidaCorrelacao() = " + r.medidaCorrelacao());
 
-        System.out.println("r.calculoEstimativa(386d) = " + r.calculoEstimativa(386d));
+
+
 
     }
 }
